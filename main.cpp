3530464432e21e6c2671dfeed3f837b0796dcf68
Maxime Lavergne <Maxime.Lavergne@bentley.com>
2018-04-19 13:43:51 -0400

    Structure of the project.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+
+int main() {
+    // add your functions under here.
+
+    // do not add anything after this line.
+    system("pause");
+    return 0;
+}
