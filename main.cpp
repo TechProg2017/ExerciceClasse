df1c2f094199ed17070063cfb68883aa51e02902
Raphael Morin <1810028@bart.ca>
2018-04-19 18:51:33 -0400

    FIRST FUNCTION AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ void LapierreKimberly1();
 void PatrickRenaud1();
 void PatrickRenaud2();
 void PatrickRenaud3();
+void RaphaelMorin1();
 
 void IvannGouletDoyon1()
 {
@@ -20,15 +21,15 @@ void IvannGouletDoyon1()
 }
 
 int main() {
-    // fill up the map with an association letter -> number 
-    // a = 1, b = 2, c = 3, etc.
-    alphabet = std::map<char, int>();
-    for (int i = 1; i < 25; i++) 
+	// fill up the map with an association letter -> number 
+	// a = 1, b = 2, c = 3, etc.
+	alphabet = std::map<char, int>();
+	for (int i = 1; i < 25; i++)
 	{
-        alphabet[char(i + 96)] = i;
-    }
+		alphabet[char(i + 96)] = i;
+	}
 
-    // add your functions under here.
+	// add your functions under here.
 
 
 	PatrickRenaud1();
@@ -40,10 +41,11 @@ int main() {
 	IvannGouletDoyon1();
 	JimmyGagnonVezina1();
 	LapierreKimberly1();
+	RaphaelMorin1();
 
-    // do not add anything after this line.
-    system("pause");
-    return 0;
+	// do not add anything after this line.
+	system("pause");
+	return 0;
 }
 
 
@@ -71,7 +73,7 @@ void LapierreKimberly1()
 }
 
 void MaillyLegareAlexandre1()
-{ 
+{
 	std::cout << "Alexandre ";
 }
 
@@ -82,4 +84,8 @@ void MaillyLegareAlexandre2()
 void JimmyGagnonVezina1()
 {
 	std::cout << "JimmyGagnonVezina" << std::endl;
+}
+
+void RaphaelMorin1() {
+	std::cout << "Raphael Morin" << std::endl;
 }
