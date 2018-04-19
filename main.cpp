9021f1299765a0dac447ea81498cf90c1bf7262b
mathieubart <mathieu_fournier99@hotmail.fr>
2018-04-19 18:08:37 -0400

    First fonction mathieu fournier

--- main.cpp
+++ main.cpp
@@ -5,17 +5,27 @@ using std::cout;
 
 std::map<char, int> alphabet;
 
-int main() {
+void MathieuFournier1();
+
+int main() 
+{
     // fill up the map with an association letter -> number 
     // a = 1, b = 2, c = 3, etc.
     alphabet = std::map<char, int>();
-    for (int i = 1; i < 25; i++) {
+    for (int i = 1; i < 25; i++) 
+	{
         alphabet[char(i + 96)] = i;
     }
 
     // add your functions under here.
+	MathieuFournier1();
 
     // do not add anything after this line.
     system("pause");
     return 0;
+}
+
+void MathieuFournier1()
+{
+	cout << "MathieuFournier" << std::endl;
 }
