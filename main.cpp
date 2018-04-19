3ccdb37583d18d7cfe25e6ff86de90184f9e4c6a
jedineeson <1810027@bart.ca>
2018-04-19 18:07:09 -0400

    Jimmy

--- main.cpp
+++ main.cpp
@@ -5,17 +5,27 @@ using std::cout;
 
 std::map<char, int> alphabet;
 
-int main() {
-    // fill up the map with an association letter -> number 
-    // a = 1, b = 2, c = 3, etc.
-    alphabet = std::map<char, int>();
-    for (int i = 1; i < 25; i++) {
-        alphabet[char(i + 96)] = i;
-    }
+void JimmyGagnonVezina1();
 
-    // add your functions under here.
+int main()
+{
+	// fill up the map with an association letter -> number 
+	// a = 1, b = 2, c = 3, etc.
+	alphabet = std::map<char, int>();
+	for (int i = 1; i < 25; i++)
+	{
+		alphabet[char(i + 96)] = i;
+	}
 
-    // do not add anything after this line.
-    system("pause");
-    return 0;
+	// add your functions under here.
+	JimmyGagnonVezina1();
+
+	// do not add anything after this line.
+	system("pause");
+	return 0;
+}
+
+void JimmyGagnonVezina1()
+{
+	std::cout << "JimmyGagnonVezina" << std::endl;
 }
