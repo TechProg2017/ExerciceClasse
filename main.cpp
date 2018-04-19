10fe1a0f5191941dcf18ede31a8584f66367b027
Raphael Morin <1810028@bart.ca>
2018-04-19 18:06:39 -0400

    FUNCTION 1 FIX

--- main.cpp
+++ main.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
+#include <map>
 
 using std::cout;
-
-int main() {
-	// add your functions under here.
-
-	// do not add anything after this line.
-	system("pause");
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+std::map<char, int> alphabet;
 
 void RaphaelMorin1() {
 	cout << "RaphaelMorin";
+}
+
+int main() {
+	// fill up the map with an association letter -> number 
+		   // a = 1, b = 2, c = 3, etc.
+	alphabet = std::map<char, int>();
+	for (int i = 1; i < 25; i++) {
+		alphabet[char(i + 96)] = i;
+
+		// add your functions under here.
+
+		// do not add anything after this line.
+		system("pause");
+		return 0;
+		RaphaelMorin1();
+	}
 }
