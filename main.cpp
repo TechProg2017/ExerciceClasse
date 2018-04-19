387e7c54c9641541d24248435d0f6b21f4169404
Maxime Lavergne <Maxime.Lavergne@bentley.com>
2018-04-19 14:03:15 -0400

    Added alphabet map.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <map>
 
 using std::cout;
 
+std::map<char, int> alphabet;
+
 int main() {
+    // fill up the map with an association letter -> number 
+    // a = 1, b = 2, c = 3, etc.
+    alphabet = std::map<char, int>();
+    for (int i = 1; i < 25; i++) {
+        alphabet[char(i + 96)] = i;
+    }
+
     // add your functions under here.
 
     // do not add anything after this line.
